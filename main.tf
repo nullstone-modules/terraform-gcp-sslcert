20304c396af322b0ab81311cfcd351b98280428a
Brad Sickles <brad.sickles@gmail.com>
2024-12-12 17:05:14 -0500

    F/2098/generalize (#1)

--- main.tf
+++ main.tf
@@ -1,27 +1,30 @@
 data "google_dns_managed_zone" "domains" {
-  for_each = var.subdomains
+  for_each = local.subdomains
 
   name = each.value
 }
 
+data "google_client_config" "this" {}
+
 locals {
-  domain_names = {for key, _ in var.subdomains : key => trimsuffix(data.google_dns_managed_zone.domains[key].dns_name, ".")}
+  domain_names = { for key, _ in local.subdomains : key => trimsuffix(data.google_dns_managed_zone.domains[key].dns_name, ".") }
 }
 
 resource "google_certificate_manager_dns_authorization" "this" {
-  for_each = var.subdomains
+  for_each = local.subdomains
 
   name        = replace(each.key, ".", "-")
-  description = "Managed by Nullstone"
+  labels      = var.labels
+  description = "${each.key}: Created by Nullstone"
   domain      = local.domain_names[each.key]
 }
 
 locals {
-  auth_records = { for key, _ in var.subdomains : key => google_certificate_manager_dns_authorization.this[key].dns_resource_record.0 }
+  auth_records = { for key, _ in local.subdomains : key => google_certificate_manager_dns_authorization.this[key].dns_resource_record.0 }
 }
 
 resource "google_dns_record_set" "authorization_records" {
-  for_each = var.subdomains
+  for_each = local.subdomains
 
   managed_zone = each.value
   name         = local.auth_records[each.key].name
@@ -32,13 +35,34 @@ resource "google_dns_record_set" "authorization_records" {
 
 resource "google_certificate_manager_certificate" "this" {
   depends_on = [google_dns_record_set.authorization_records]
+  count      = var.enabled ? 1 : 0
 
-  name        = var.cert_name
-  description = "Managed by Nullstone"
-  scope       = "EDGE_CACHE"
+  name        = var.name
+  labels      = var.labels
+  description = "Created by Nullstone"
+  scope       = var.scope == "" ? "DEFAULT" : var.scope
 
   managed {
-    domains            = [ for da in google_certificate_manager_dns_authorization.this : da.domain ]
-    dns_authorizations = [ for da in google_certificate_manager_dns_authorization.this : da.id ]
+    domains            = [for da in google_certificate_manager_dns_authorization.this : da.domain]
+    dns_authorizations = [for da in google_certificate_manager_dns_authorization.this : da.id]
   }
+}
+
+resource "google_certificate_manager_certificate_map" "this" {
+  count = var.enabled ? 1 : 0
+
+  name        = var.name
+  labels      = var.labels
+  description = "${var.name}: Created by Nullstone"
+}
+
+resource "google_certificate_manager_certificate_map_entry" "this" {
+  count = var.enabled ? 1 : 0
+
+  name         = var.name
+  labels       = var.labels
+  description  = "${var.name}: Created by Nullstone"
+  map          = google_certificate_manager_certificate_map.this[count.index].name
+  certificates = [google_certificate_manager_certificate.this[count.index].id]
+  matcher      = "PRIMARY"
 }
--- variables.tf
+++ variables.tf
@@ -4,7 +4,7 @@ variable "enabled" {
   description = "This enables/disables creating the SSL certificate. We cannot use module count because we have aliased providers."
 }
 
-variable "cert_name" {
+variable "name" {
   type        = string
   description = <<EOF
 A user-defined name of the certificate.
@@ -21,4 +21,32 @@ A list of subdomains represented as a map to register on the SSL certificate.
 In each key/value pair, the key refers to the DNS name of the subdomain and the value refers to the GCP unique zone name of the domain.
 If you want to use the dns_name from the domain zone, use `.` for the map key.
 EOF
+}
+
+locals {
+  subdomains = var.enabled ? var.subdomains : tomap({})
+}
+
+variable "labels" {
+  type        = map(string)
+  default     = {}
+  description = "A map of labels to apply to certificate resources"
+}
+
+variable "scope" {
+  type        = string
+  default     = ""
+  description = <<EOF
+The scope of the certificate relies on how the certificate will be used.
+- "": (default) For use with Load Balancing and Cloud CDN
+- "ALL_REGIONS": For use with cross-region internal application load balancing
+- "EDGE_CACHE": For use with media edge services
+EOF
+
+  validation {
+    condition     = contains(["EDGE_CACHE", "ALL_REGIONS", ""], var.scope)
+    error_message = <<EOF
+scope must be one of "EDGE_CACHE", "ALL_REGIONS", or ""
+EOF
+  }
 }
