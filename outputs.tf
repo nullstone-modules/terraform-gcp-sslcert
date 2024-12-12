efcafb31c3f07f82617f268b6a80db225bceda00
Brad Sickles <brad.sickles@gmail.com>
2024-12-12 17:32:17 -0500

    Added `certificate_map_name` output

--- outputs.tf
+++ outputs.tf
@@ -8,6 +8,11 @@ output "certificate_id" {
   description = "The ID of the certificate in GCP. (Format: projects/{{project}}/locations/global/certificates/{{name}})"
 }
 
+output "certificate_map_name" {
+  value       = try(google_certificate_manager_certificate_map.this[0].name, "")
+  description = "The name of the certificate map in GCP Certificate Manager."
+}
+
 output "certificate_map_id" {
   value       = try(google_certificate_manager_certificate_map.this[0].id, "")
   description = "The ID of the certificate map in GCP Certificate Manager. (Format: projects/{{project}}/locations/global/certificateMaps/{{name}})"
